ea5884d8c989804e55adeb2c896ac4f27a55d372
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-04-29 21:32:00 +0600

    Project setup complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Epilogue, Geist, Geist_Mono, Manrope } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const manrope = Manrope({
+  variable: "--font-man",
   subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const epilogue = Epilogue({
+  variable: "--font-epi",
   subsets: ["latin"],
 });
 
@@ -20,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${manrope.variable} ${epilogue.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
